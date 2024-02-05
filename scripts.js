dfd8c29fd97d413cd541362a8107386c2f8d883c
gabrielfloss <gbgabrielr1@gmail.com>
2024-02-05 17:04:43 -0300

    sorteiador

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function generatedNumber(){
+    
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+     const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+     alert(result)
+    
+    
+
+}
